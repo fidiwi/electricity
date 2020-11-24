29e72906e6cac74d7e4971ae151b3408c1604039
KiKoLEL <kianmars177@gmail.com>
2020-11-24 19:23:55 +0100

    moin

--- steuerung/verwaltung.py
+++ steuerung/verwaltung.py
@@ -1,5 +1,5 @@
 import time
-import hardware
+#import hardware
 
 
 class House:
@@ -43,21 +43,28 @@ class Storage():
             self.capacity = self.min
             print("empty")
 
+
 class Firma(House):
     def __init__(self):
-        super().__init__(max_consumption=4, solar_space=8.75)
+        super().__init__(max_consumption=20, solar_space=20) #Zahlen einsetzen für den Verbrauch
 
+
+class Windpark():
+    def __init__(self):
+        self.windenergy = 50 #maximale Produktion
 
 
 if __name__ == "__main__":
     houses = {}
     storage = Storage(200, 0, 350)
 
-    verbrauch_haus = hardware.getAnalogPercent(0)  # 0.5
-    erzeugung_haus = hardware.getAnalogPercent(1)  # 0.2
-    verbrauch_firma = hardware.getAnalogPercent(2)  
-    erzeugung_firma = hardware.getAnalogPercent(3)
-    preis_vorhersage = hardware.getAnalogPercent(4)
+    verbrauchfirma = 0
+
+    verbrauch_haus = 0.5 #hardware.getAnalogPercent(0) 
+    erzeugung_solar = 0.25 #hardware.getAnalogPercent(1)  
+    verbrauch_firma = 1 #hardware.getAnalogPercent(2)  
+    erzeugung_wind = 0.5 #hardware.getAnalogPercent(3)
+    preis_vorhersage = 0 #hardware.getAnalogPercent(4)
     charge_cars = 15 # wie viele Autos gerade aufgeladen werden
     hours = 0
 
@@ -67,6 +74,7 @@ if __name__ == "__main__":
     houses[4] = Einfamilienhaus()
     houses[5] = Mehrfamilienhaus()
     firma = Firma()
+    windpark = Windpark()
     try:
 
         while True:
@@ -74,7 +82,7 @@ if __name__ == "__main__":
             total_dif = 0 # Verbrauch der Siedlung
             for house_key in houses:
                 house_verbrauch = houses[house_key].max_consumption * verbrauch_haus + charge_cars * 0.04
-                house_erzeugung = houses[house_key].solar_space * erzeugung_haus
+                house_erzeugung = houses[house_key].solar_space * erzeugung_solar
                 difference = house_erzeugung - house_verbrauch
                 dif[house_key] = difference
                 total_dif = total_dif + difference
@@ -85,7 +93,10 @@ if __name__ == "__main__":
             time.sleep(1)
             hours+=1
             print(hours)
-            total_dif = total_dif + firma.max_consumption * verbrauch_firma - firma.solar_space * erzeugung_firma
-            print(total_dif)
+            verbrauchfirma = firma.solar_space * erzeugung_solar + windpark.windenergy * erzeugung_wind - firma.max_consumption * verbrauch_firma
+            print("Firma ",verbrauchfirma)
+            total_dif = total_dif + verbrauchfirma        
+            print("Endverbrauch ",total_dif)
+
     except KeyboardInterrupt:
         pass
