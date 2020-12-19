66add5aaed9b15324ec1ae8c9c05a0aaf08813e0
KiKoLEL <kianmars177@gmail.com>
2020-12-19 14:23:02 +0100

    mit firma und wind in der liste

--- steuerung/hardware.py
+++ steuerung/hardware.py
@@ -33,7 +33,7 @@ wind = [*range(8, 17)]
 firma = [*range(28, 37)]
 sun = [*range(139, 179)]
 
-ways = {1: house1, 2: house2, 3: house3, 4: storage, 5: house5}
+ways = {1: house1, 2: house2, 3: house3, 4: storage, 5: house5, 6:firma, 7:wind}
 
 
 # Darf keine __init__() haben, sonst hat sich bisher alles weiß geschaltet!
