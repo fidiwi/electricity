500e762a70483a3966c64d259fe4dc55b97e751b
fidiwi <73874736+fidiwi@users.noreply.github.com>
2020-12-21 16:35:02 +0100

    erste versuche

--- app/jufo/src/App.tsx
+++ app/jufo/src/App.tsx
@@ -36,30 +36,8 @@ import './theme/variables.css';
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+    <Tab1>
+    </Tab1>
   </IonApp>
 );
 
--- app/jufo/src/pages/Tab1.tsx
+++ app/jufo/src/pages/Tab1.tsx
@@ -1,23 +1,96 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useRef, useState } from 'react';
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonProgressBar, IonRange, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { analytics } from 'ionicons/icons'
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
+
+
 const Tab1: React.FC = () => {
+  const rangeElement = useRef<HTMLIonRangeElement>(null);
+  
+  const [moin, setVal] = useState<number>(0);
+
+  const setRange = () => {
+    setVal(+rangeElement.current!.value);
+  };
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle>
+              So ein geiles Haus
+            </IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonContent>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <IonCard>
+                  <IonIcon color="primary" size="large" icon={analytics}></IonIcon>
+                    <IonCardHeader>
+                      <IonCardSubtitle>Der aktuelle Preis</IonCardSubtitle>
+                      <IonCardTitle>30 Cent
+                    </IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+              <IonCol>
+                <IonCard>
+                  <IonIcon color="primary" size="large" icon={analytics}></IonIcon>
+                    <IonCardHeader>
+                      <IonCardSubtitle>Der aktuelle Preis</IonCardSubtitle>
+                      <IonCardTitle>30 Cent
+                    </IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonCard>
+                  <IonIcon slot="start" color="primary" size="large" icon={analytics}></IonIcon>
+                  <IonCardHeader>
+                    <IonCardSubtitle>Der aktuelle Preis</IonCardSubtitle>
+                    <IonCardTitle>30 Cent
+                    </IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+              <IonCol>
+                <IonCard>
+                  <IonIcon color="primary" size="large" icon={analytics}></IonIcon>
+                    <IonCardHeader>
+                      <IonCardSubtitle>Der aktuelle Preis</IonCardSubtitle>
+                      <IonCardTitle>30 Cent
+                    </IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+              <IonRow>
+                <IonCol>
+                  <progress className="bar" max={100} value={moin}/>
+                </IonCol>
+                <IonCol>
+                  <h2>djflajldf <IonIcon name="accesability"></IonIcon> </h2>
+                </IonCol>
+                <IonCol>
+                  <div className="bar">
+                    <IonProgressBar color="secondary" value={moin/100}>
+                      <IonLabel slot="start">Moin</IonLabel>
+                    </IonProgressBar>
+                  </div>
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonRange ref={rangeElement} min={0} max={100} color="secondary" onIonChange={setRange}>
+                </IonRange>
+              </IonRow>
+            </IonGrid>
+        </IonContent>
       </IonContent>
     </IonPage>
   );
