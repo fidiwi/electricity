ae1a1523918c07da3dd5b71c84287aaaa8435235
fidiwi <73874736+fidiwi@users.noreply.github.com>
2020-12-23 20:04:16 +0100

    lol

--- app/jufo/node_modules/@ionic/react-hooks/util/models.d.ts
+++
@@ -1,9 +0,0 @@
-export interface AvailableResult {
-    isAvailable: boolean;
-}
-export declare const notAvailable: {
-    isAvailable: boolean;
-};
-export declare class FeatureNotAvailableError extends Error {
-    constructor();
-}
