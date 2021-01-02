4dc8dd722b26ee0e7823a1fe524f24fc69f67b4d
fidiwi <73874736+fidiwi@users.noreply.github.com>
2021-01-02 17:40:17 +0100

    als kian bei mir war

--- steuerung/verwaltung.py
+++ steuerung/verwaltung.py
@@ -10,36 +10,54 @@ LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 30     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
 class House:
-    def __init__(self, max_consumption, solar_space, slot):
+    def __init__(self, max_consumption, solar_space, slot, carsCurrentlyCharging):
         self.max_consumption = max_consumption  # Maximaler Verbrauch in kW(h)
         self.solar_space = solar_space  # Solarenergie -> Leistung
         self.way = hardware.ways[slot]  # Wähle den Weg des zugehörigen Slots
+        self.slot = slot
+        self.carsCurrentlyCharging = carsCurrentlyCharging
+    
+    def setCarsCharging(self, carsCharging):
+        self.carsCurrentlyCharging = carsCharging
 
 
 class Apartment(House):
     def __init__(self, slot):
-        super().__init__(5.7, 12.5, slot)
+        super().__init__(5.7, 12.5, slot, 0)
 
 
 class Einfamilienhaus(House):
     def __init__(self, slot):
-        super().__init__(1.2, 18.75, slot)
+        super().__init__(1.2, 18.75, slot, 0)
 
 
 class Reihenhaus(House):
     def __init__(self, slot):
-        super().__init__(1.5, 3.75, slot)
+        super().__init__(1.5, 3.75, slot, 0)
 
 
 class Mehrfamilienhaus(House):
     def __init__(self, slot):
-        super().__init__(4, 8.75, slot)
+        super().__init__(4, 8.75, slot, 0)
+
+
+class Firma(House):
+    def __init__(self, slot):
+        super().__init__(20, 20, slot, 0)  # Zahlen für den
+        # Verbrauch einsetzen
+
+
+class Windpark():
+    def __init__(self, slot):
+        self.windenergy = 10  # maximale Produktion
+        self.way = hardware.ways[slot]
+        self.slot = slot
 
 
 class Storage():
@@ -61,18 +79,6 @@ class Storage():
             print("empty")
 
 
-class Firma(House):
-    def __init__(self, slot):
-        super().__init__(20, 20.1, slot)  # Zahlen für den
-        # Verbrauch einsetzen
-
-
-class Windpark():
-    def __init__(self):
-        self.windenergy = 50  # maximale Produktion
-        self.way = hardware.ways[6]
-
-
 def speed(dif):
     if abs(dif) > 10:
         dif = 10
@@ -87,7 +93,7 @@ def calcled(i, j, vb_sortiert, keys):  # i = erstes haus von links; j = rechtes 
     speedSR = 0
     speedTeiler = 1
 
-    if vb_sortiert[keys[i]] > 0 and vb_sortiert[keys[j]] < -1:  # Wenn i erzeugt und j verbraucht
+    if vb_sortiert[keys[i]] >= 0 and vb_sortiert[keys[j]] < 0:  # Wenn i erzeugt und j verbraucht
         summe = 0
         if vb_sortiert[keys[i]] + vb_sortiert[keys[j]] > 0:  # Wenn i den Verbrauch von j mehr als decken kann
             # em = j
@@ -96,12 +102,12 @@ def calcled(i, j, vb_sortiert, keys):  # i = erstes haus von links; j = rechtes 
                     summe += abs(vb_sortiert[keys[j]])
                     speedSR += vb_sortiert[keys[j]]
                     speedTeiler += 1
-                    receiver += [houses[j].way]
+                    receiver += [keys[j].way]
                     print("receiver: ", receiver)
                     j -= 1
                 else:
                     break
-            ledStrip.stromfluss(Color(0, 50, 0), speed(speedSR/speedTeiler), houses[i].way, receiver)
+            ledStrip.stromfluss(Color(0, 50, 0), speed(speedSR/speedTeiler), keys[i].way, receiver)
             vb_sortiert[keys[i]] -= summe  # Erzeugung von i mit dem Verbrauch von j subtrahieren
             # j -= 1  # Springe zum nächsten verbrauchenden Haus
             if not i > j:
@@ -112,48 +118,49 @@ def calcled(i, j, vb_sortiert, keys):  # i = erstes haus von links; j = rechtes 
                     summe += abs(vb_sortiert[keys[i]])
                     speedSR += vb_sortiert[keys[i]]
                     speedTeiler += 1
-                    sender += [houses[i].way]
+                    sender += [keys[i].way]
                     print("sender: ", sender)
                     i += 1
                 else:
                     break
-            ledStrip.stromfluss(Color(0, 50, 0), speed(speedSR/speedTeiler), sender, houses[j].way)
+            ledStrip.stromfluss(Color(0, 50, 0), speed(speedSR/speedTeiler), sender, keys[j].way)
             vb_sortiert[keys[j]] += summe  # Verbrauch von j mit der Erzeugung von i senken
             # i += 1  # Springe zum nächsten erzeugenden Haus
             if not i > j:
                 calcled(i, j, vb_sortiert, keys)
 
-    elif vb_sortiert[keys[i]] > 0 and vb_sortiert[keys[j]] > 0:  # Wenn i und j erzeugen
+    elif vb_sortiert[keys[i]] >= 0 and vb_sortiert[keys[j]] >= 0:  # Wenn i und j erzeugen
         if storage.capacity < 350:
             while not i > j:  # j+1, da auch das house[j] angezeigt werden muss
-                if not i == 3:
+                if not keys[i].slot == 3:
                     speedSR += vb_sortiert[keys[i]]
                     speedTeiler += 1
-                    sender += [houses[i].way]
+                    sender += [keys[i].way]
                 i += 1
             ledStrip.stromfluss(Color(0, 150, 50), speed(speedSR/speedTeiler), sender, houses[3].way)
         else:
             while not i > j:
                 speedSR += vb_sortiert[keys[i]]
                 speedTeiler += 1
-                sender += [houses[i].way]
+                sender += [keys[i].way]
                 i += 1
             ledStrip.stromfluss(Color(0, 0, 50), speed(speedSR/speedTeiler), sender, hardware.end)
 
     else:  # Wenn beide verbrauchen
         if storage.capacity > 0:
             while not i > j:
-                if not i == 3:
+                if not (keys[i].slot == 3 or keys[i].slot == 6):
                     speedSR += vb_sortiert[keys[i]]
                     speedTeiler += 1
-                    receiver += [houses[i].way]
+                    receiver += [keys[i].way]
                 i += 1
             ledStrip.stromfluss(Color(80, 50, 0), speed(speedSR/speedTeiler), houses[3].way, receiver)
         else:
             while not i > j:
-                speedSR += vb_sortiert[keys[i]]
-                speedTeiler += 1
-                receiver += [houses[i].way]
+                if not keys[i].slot == 6:
+                    speedSR += vb_sortiert[keys[i]]
+                    speedTeiler += 1
+                    receiver += [keys[i].way]
                 i += 1
             ledStrip.stromfluss(Color(50, 0, 0), speed(speedSR/speedTeiler), hardware.begin, receiver)
 
@@ -184,11 +191,11 @@ if __name__ == "__main__":
 
     verbrauch_haus = hardware.getAnalogPercent(0)  # 0.5
     erzeugung_solar = hardware.getAnalogPercent(1)  # 0.25
-    verbrauch_firma = hardware.getAnalogPercent(2)  # 1
+    #verbrauch_firma = hardware.getAnalogPercent(2)  # 1
     erzeugung_wind = hardware.getAnalogPercent(3)  # 0.5
     preis_vorhersage = hardware.getAnalogPercent(4)  # 0
-    charge_cars = 15  # wie viele Autos gerade aufgeladen werden
     hours = 0
+    ladeleistung = 11  # in kW 
 
     # Häuser mit jeweiligen Slots, jeder Slot nur einmal!!
     houses[0] = Einfamilienhaus(0)
@@ -196,9 +203,8 @@ if __name__ == "__main__":
     houses[2] = Reihenhaus(2)
     houses[3] = Apartment(3)
     houses[4] = Mehrfamilienhaus(4)
-    firma = Firma(5)
-    windpark = Windpark()
-    houses[5] = firma
+    houses[5] = Firma(5)
+    windpark = Windpark(6)
 
     ledStrip = hardware.LEDStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ,
                                  LED_DMA, LED_INVERT, LED_BRIGHTNESS,
@@ -209,7 +215,7 @@ if __name__ == "__main__":
 
         while True:
             updatePotiValues()
-            dif = {}
+            """dif = {}
             total_dif = 0  # Verbrauch der Siedlung
             for house_key in houses:
                 house_verbrauch = houses[house_key].max_consumption * verbrauch_haus
@@ -228,22 +234,63 @@ if __name__ == "__main__":
             - firma.max_consumption * verbrauch_firma
             print("Firma ", verbrauchfirma)
             total_dif += verbrauchfirma
-            print("Endverbrauch ", total_dif)
-            total_dif += charge_cars * 0.04
+            print("Endverbrauch ", total_dif)"""
 
+            houses[1].setCarsCharging(1)
             # led rechnen
             housevb = []
-            for i in range(6):
-                housevb.append(-(houses[i].max_consumption * verbrauch_haus + charge_cars * 0.04) + houses[i].solar_space * erzeugung_solar)
+            for i in range(5):
+                housevb.append(-(houses[i].max_consumption * verbrauch_haus + houses[i].carsCurrentlyCharging * ladeleistung) + houses[i].solar_space * erzeugung_solar)
+            # Windpark
+            housevb.append(windpark.windenergy * erzeugung_wind)
 
-            dic = {houses[0]: housevb[0], houses[1]: housevb[1], houses[2]: housevb[2], houses[3]: housevb[3], houses[4]: housevb[4], houses[5]: housevb[5]}
+            dif = houses[5].solar_space * erzeugung_solar
+            for vb in housevb:
+                dif += vb
+            
+            print("dif: ", dif)
+            verbrauch_firma = 0.5
+            if dif > 10:
+                verbrauch_firma = dif/houses[5].max_consumption
+                if dif > houses[5].max_consumption:
+                    verbrauch_firma = 1
+            print("verbrauch_firma: ", verbrauch_firma)
+
+            # Firma
+            housevb.append(-(houses[5].max_consumption * verbrauch_firma) + houses[5].solar_space * erzeugung_solar)
+            
+
+            dic = {houses[0]: housevb[0], houses[1]: housevb[1], houses[2]: housevb[2], houses[3]: housevb[3], houses[4]: housevb[4], houses[5]: housevb[6], windpark: housevb[5]}
             vb_sortiert = {k: v for k, v in sorted(dic.items(), key=lambda item: item[1], reverse=True)}
-            keys = list(vb_sortiert.keys())     
+            
+            print(dic)
+            # Items mit VB = 0
+            keys_with_zero = []
+            for key in vb_sortiert.keys():
+                if vb_sortiert[key] == 0:
+                    keys_with_zero.append(key)
+            for key in keys_with_zero:
+                del vb_sortiert[key]
+            
+            keys = list(vb_sortiert.keys())
 
-            calcled(0, 5, vb_sortiert, keys)
+            total_dif = 0  # Verbrauch der Siedlung
+
+            for items in vb_sortiert.values():
+                total_dif += items
+                print(items)
+            storage.startCharging(total_dif)
+
+            print("storage: ", storage.capacity)
+            print("Totale Differenz: ", total_dif)
+            print(vb_sortiert)
+         
+
+
+            calcled(0, len(vb_sortiert) - 1, vb_sortiert, keys)
 
             # hardware.sonne(erzeugung_solar)
-
+            hours += 1
             time.sleep(2)
 
     except KeyboardInterrupt:
