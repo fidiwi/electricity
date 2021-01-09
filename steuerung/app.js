67327b2b4829007ee0bcea8f0f2f50b0f36cb4c9
fidiwi <73874736+fidiwi@users.noreply.github.com>
2021-01-09 15:30:31 +0100

    sdf

--- steuerung/app.js
+++ steuerung/app.js
@@ -1,12 +1,39 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const mysql = require('mysql');
 const config = require("./config.json");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server); // < Interesting!
+
+const getApiAndEmit = "TODO";
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: config.user,
   password: config.password,
   database: config.database
 });
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+    socket.emit("FromAPI", "lol");
+    io.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
+
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
+
+server.listen(port, () => console.log(`Listening on Port ${port}`))
