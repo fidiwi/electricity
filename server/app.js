2e162d42571464c45aff4e3992657452f3f4b225
fidiwi <73874736+fidiwi@users.noreply.github.com>
2021-01-13 11:30:47 +0100

    Send on update

--- server/app.js
+++ server/app.js
@@ -27,19 +27,37 @@ const SQLconnection = mysql.createConnection({
   database: config.database
 });
 
+var manipulationSockets = [];
+
 io.on("connection", (socket) => {
     console.log("New client connected");
 
+    // Jeweilige Client-Art abfragen und in Kategorien speichern
     socket.on("manipulation", () => {
-      SQLconnection.query("SELECT * FROM sliders", (err, rows) => {
-        if (err) throw err;
-        socket.emit("FromAPI", {housevb: rows[0].housevb, companyvb: rows[0].companyvb, sun: rows[0].sun, wind: rows[0].wind, ekarma: rows[0].ekarma});
-      });
-      socket.on("rangeChange", (data) =>{
-        const param = data.param;
-        const value = data.value;
+      manipulationSockets.push(socket);
+      startManipulationSocket(socket);
 
-        SQLconnection.query(`UPDATE sliders SET ${param} = ${value} WHERE id=1`, (err) => {if (err) throw err;});
+      socket.on("disconnect", () => {
+        console.log("Manipulation disconnected");
+
+        const index = manipulationSockets.indexOf(socket);
+        if (index > -1) {
+          manipulationSockets.splice(index, 1);
+        }
+      });
+    });
+
+    socket.on("raspberry", () => {
+      manipulationSockets.push(socket);
+      startManipulationSocket(socket);
+
+      socket.on("disconnect", () => {
+        console.log("Raspberry Pi disconnected");
+
+        const index = manipulationSockets.indexOf(socket);
+        if (index > -1) {
+          manipulationSockets.splice(index, 1);
+        }
       });
     });
 
@@ -54,17 +72,46 @@ io.on("connection", (socket) => {
         // Object ausgeben nach Format {0: 0.3, ..., 23: 0.5}
         socket.emit("FromAPI", entries);
       });
+
+      socket.on("disconnect", () => {
+        console.log("HouseVB client disconnected");
+      });
     });
 
-
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-    });
 });
 
 SQLconnection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
+
+function startManipulationSocket(socket){
+  // Am Anfang die Werte senden
+  sendSliders(socket);
+
+  // Wenn Sliderwerte in der DB geänder werden sollen
+  socket.on("rangeChange", (data) =>{
+    const param = data.param;
+    const value = data.value;
+
+    SQLconnection.query(`UPDATE sliders SET ${param} = ${value} WHERE id=1`, (err) => {if (err) throw err;});
+
+    manipulationSockets.forEach(function(manipulationSocket){
+      // Jedem anderen "sliders"-Client aktuelle Werte schicken
+      if(manipulationSocket !== socket){
+        sendSliders(manipulationSocket);
+      }
+    })
+  });
+
+}
+
+// Sende Sliderdaten an den jeweiligen Socket
+function sendSliders(socket){
+  SQLconnection.query("SELECT * FROM sliders", (err, rows) => {
+    if (err) throw err;
+    socket.emit("FromAPI", {housevb: rows[0].housevb, companyvb: rows[0].companyvb, sun: rows[0].sun, wind: rows[0].wind, ekarma: rows[0].ekarma});
+  });
+}
 
 server.listen(port, () => console.log(`Listening on Port ${port}`))
