088e14bef40248f3eeb67ff8b1bea22ebfd0fd8f
fidiwi <73874736+fidiwi@users.noreply.github.com>
2021-01-13 10:44:22 +0100

    rangeChange socket use state

--- app/jufo/src/pages/ModelManipulation.tsx
+++ app/jufo/src/pages/ModelManipulation.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonRange, IonLabel, IonIcon, IonItemDivider, IonBackButton, IonButtons, IonTextarea } from '@ionic/react';
 import { analytics, cloud, colorFill, sunny, business, home } from 'ionicons/icons';
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 import './ModelManipulation.css';
 import { urls } from "../vars/vars";
 
@@ -15,9 +15,11 @@ const ModelManipulation: React.FC = () => {
 
   const [response, setResponse] = useState<any>({housevb:0, companyvb:0, sun:0, wind:0, ekarma:0});
 
-  const socket = io(urls.SOCKET_ENDPOINT);
+  const [currentSocket, setSocket] = useState(io());
 
   useEffect(() => {
+    const socket = io(urls.SOCKET_ENDPOINT);
+    setSocket(socket);
     socket.emit("manipulation");
     socket.on("FromAPI", (data: any) => {
       setResponse(data);
@@ -35,7 +37,7 @@ const ModelManipulation: React.FC = () => {
   }>({ lower: 0, upper: 0 });
 
   const pushRangeChange = (rangeElement: React.RefObject<HTMLIonRangeElement>) => {
-    socket.emit("rangeChange", rangeElement.current!.value);
+    currentSocket.emit("rangeChange", {param: rangeElement.current!.name, value: rangeElement.current!.value});
   };
 
   return (
@@ -52,35 +54,35 @@ const ModelManipulation: React.FC = () => {
         <IonList>
           <IonItemDivider>Häuserverbauch</IonItemDivider>
           <IonItem>
-            <IonRange ref={housevbRange} min={0} max={1} step={0.05} color="danger" snaps={true} value={response.housevb} onIonChange={() => {pushRangeChange(housevbRange);}} >
+            <IonRange ref={housevbRange} name="housevb" min={0} max={1} step={0.05} color="danger" snaps={true} value={response.housevb} onIonChange={() => {pushRangeChange(housevbRange)}} >
               <IonIcon size="small" slot="start" icon={home} />
               <IonIcon slot="end" icon={home} />
             </IonRange>
           </IonItem>
           <IonItemDivider>Sonne</IonItemDivider>
           <IonItem>
-            <IonRange ref={sunRange} min={0} max={1} step={0.01} value={response.sun} onIonChange={() => {pushRangeChange(sunRange);}} >
+            <IonRange ref={sunRange} name="sun" min={0} max={1} step={0.01} value={response.sun} onIonChange={() => {pushRangeChange(sunRange);}} >
               <IonIcon size="small" slot="start" icon={sunny}/>
               <IonIcon slot="end" icon={sunny} />
             </IonRange>
           </IonItem>
           <IonItemDivider>Firmaverbrauch</IonItemDivider>
           <IonItem>
-            <IonRange ref={companyvbRange} min={0} max={1} step={0.01} value={response.companyvb} onIonChange={() => {pushRangeChange(companyvbRange);}} >
+            <IonRange ref={companyvbRange} name="companyvb" min={0} max={1} step={0.01} value={response.companyvb} onIonChange={() => {pushRangeChange(companyvbRange);}} >
               <IonIcon size="small" slot="start" icon={business}/>
               <IonIcon slot="end" icon={business} />
             </IonRange>
           </IonItem>
           <IonItemDivider>Wind</IonItemDivider>
           <IonItem>
-            <IonRange ref={windRange} min={0} max={1} step={0.01} value={response.wind} onIonChange={() => {pushRangeChange(windRange);}} >
+            <IonRange ref={windRange} name="wind" min={0} max={1} step={0.01} value={response.wind} onIonChange={() => {pushRangeChange(windRange);}} >
               <IonIcon size="small" slot="start" icon={cloud} />
               <IonIcon slot="end" icon={cloud} />
             </IonRange>
           </IonItem>
           <IonItemDivider>Vorhersage</IonItemDivider>
           <IonItem>
-            <IonRange ref={ekarmaRange} min={0} max={1} step={0.01} value={response.ekarma} onIonChange={() => {pushRangeChange(ekarmaRange);}} >
+            <IonRange ref={ekarmaRange} name="ekarma" min={0} max={1} step={0.01} value={response.ekarma} onIonChange={() => {pushRangeChange(ekarmaRange);}} >
               <IonIcon size="small" slot="start" icon={analytics} />
               <IonIcon slot="end" icon={analytics} />
             </IonRange>
