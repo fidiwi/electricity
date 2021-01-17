cb1c94f8e9e731fbbd83b5e1293f08fd2389391e
KiKoLEL <kianmars177@gmail.com>
2021-01-17 11:48:29 +0100

    kWh

--- app/jufo/src/pages/Consum.tsx
+++ app/jufo/src/pages/Consum.tsx
@@ -30,14 +30,14 @@ const Consum: React.FC = () => {
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
     datasets: [
       {
-        label: "Stromverbrauch in kW",
+        label: "Stromverbrauch in kWh",
         data: [Math.round(416000*vbr)/100, Math.round(420000*vbr)/100, Math.round(418000*vbr)/100, Math.round(419000*vbr)/100, 4170*vbr, Math.round(415000*vbr)/100, Math.round(416000*vbr)/100, Math.round(414000*vbr)/100, Math.round(415500*vbr)/100, Math.round(416000*vbr)/100, Math.round(416000*vbr)/100, Math.round(417000*vbr)/100],
         fill: false,
         backgroundColor: "rgba(75,192,192,0.2)",
         borderColor: "rgba(204,0,0,1)"
       },
       {
-        label: "Stromproduktion in kW",
+        label: "Stromproduktion in kWh",
         data: [Math.round(2337*prd)/100, Math.round(6352*prd)/100, Math.round(6423*prd)/100, Math.round(12767*prd)/100, Math.round(11986*prd)/100, Math.round(14608*prd)/100, Math.round(12530*prd)/100, Math.round(13039*prd)/100, Math.round(9442*prd)/100, Math.round(6350*prd)/100, Math.round(2870*prd)/100, Math.round(1970*prd)/100],
         fill: true,
         backgroundColor: "rgba(0,204,0,0.2)",
@@ -59,14 +59,14 @@ const Consum: React.FC = () => {
     labels: ["01", "04", "07", "10", "13", "16", "19", "22", "25", "28", "31"],
     datasets: [
       {
-        label: "Stromverbrauch in kW",
+        label: "Stromverbrauch in kWh",
         data: [Math.round(41800*vbr)/100, Math.round(42000*vbr)/100, Math.round(41900*vbr)/100, Math.round(41600*vbr)/100, Math.round(41500*vbr)/100, Math.round(41700*vbr)/100, Math.round(41600*vbr)/100, Math.round(41400*vbr)/100, Math.round(41550*vbr)/100, Math.round(41600*vbr)/100, Math.round(41600*vbr)/100, Math.round(41700*vbr)/100],
         fill: false,
         backgroundColor: "rgba(75,192,192,0.2)",
         borderColor: "rgba(204,0,0,1)"
       },
       {
-        label: "Stromproduktion in kW",
+        label: "Stromproduktion in kWh",
         data: [Math.round(980*prd)/100, Math.round(1730*prd)/100, Math.round(1880*prd)/100, Math.round(1120*prd)/100, Math.round(1360*prd)/100, Math.round(1500*prd)/100, Math.round(1190*prd)/100, Math.round(780*prd)/100, Math.round(1440*prd)/100, Math.round(1880*prd)/100, Math.round(580*prd)/100],
         fill: true,
         backgroundColor: "rgba(0,204,0,0.2)",
@@ -90,14 +90,14 @@ const Consum: React.FC = () => {
     labels: ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"],
     datasets: [
       {
-        label: "Stromverbrauch in kW",
+        label: "Stromverbrauch in kWh",
         data: [Math.round(13300*vbr)/100, Math.round(13500*vbr)/100, Math.round(13400*vbr)/100, Math.round(13700*vbr)/100, Math.round(13900*vbr)/100, Math.round(14100*vbr)/100, Math.round(13900*vbr)/100],
         fill: false,
         backgroundColor: "rgba(75,192,192,0.2)",
         borderColor: "rgba(204,0,0,1)"
       },
       {
-        label: "Stromproduktion in kW",
+        label: "Stromproduktion in kWh",
         data: [Math.round(400*prd)/100, Math.round(580*prd)/100, Math.round(460*prd)/100, Math.round(650*prd)/100, Math.round(630*prd)/100, Math.round(600*prd)/100, Math.round(580*prd)/100],
         fill: true,
         backgroundColor: "rgba(0,204,0,0.2)",
@@ -165,14 +165,14 @@ const Consum: React.FC = () => {
         labels: ["00", "01", "02", "03", "04", "05","06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
         datasets: [
           {
-            label: "SVB",
+            label: "Stromverbrauch in kWh",
             data: hourList,
             fill: false,
             backgroundColor: "rgba(75,192,192,0.2)",
             borderColor: "rgba(204,0,0,1)"
           },
           {
-            label: "Produktion",
+            label: "Stromproduktion in kWh",
             data: [33, 25, 35, 51, 54, 76, 33, 53, 85, 41, 44, 65, 23, 33, 25, 35, 51, 54, 76, 33, 53, 85, 41, 44, 65, 23],
             fill: true,
             backgroundColor: "rgba(0,204,0,0.2)",
@@ -211,9 +211,9 @@ const Consum: React.FC = () => {
               <IonCardHeader>
                 <IonCardTitle>Tagesüberblick</IonCardTitle>
                 <Line data={dataTag} legend={legend}/>
-                <IonCardSubtitle>Stromverbrauch: {Math.round(vbr*137)}kW</IonCardSubtitle>
-                <IonCardSubtitle>Stromproduktion: {Math.round(prd*6)}kW</IonCardSubtitle>
-                <IonCardSubtitle>Differenz: 12kW</IonCardSubtitle>
+                <IonCardSubtitle>Stromverbrauch: {Math.round(vbr*137)}kWh</IonCardSubtitle>
+                <IonCardSubtitle>Stromproduktion: {Math.round(prd*6)}kWh</IonCardSubtitle>
+                <IonCardSubtitle>Differenz: 12kWh</IonCardSubtitle>
               </IonCardHeader>
             </IonCard>
             <IonCard>
@@ -222,9 +222,9 @@ const Consum: React.FC = () => {
                 <Line data={dataWoche}/>
               </IonCardHeader>
               <IonCardContent>
-                <IonCardSubtitle>Stromverbrauch: {Math.round(vbr*959)}kW</IonCardSubtitle>
-                <IonCardSubtitle>Stromproduktion: {Math.round(prd*39)}kW</IonCardSubtitle>
-                <IonCardSubtitle>Differenz: {Math.round(prd*39)-Math.round(vbr*959)}kW</IonCardSubtitle>
+                <IonCardSubtitle>Stromverbrauch: {Math.round(vbr*959)}kWh</IonCardSubtitle>
+                <IonCardSubtitle>Stromproduktion: {Math.round(prd*39)}kWh</IonCardSubtitle>
+                <IonCardSubtitle>Differenz: {Math.round(prd*39)-Math.round(vbr*959)}kWh</IonCardSubtitle>
               </IonCardContent>
             </IonCard>
             <IonCard>
@@ -233,9 +233,9 @@ const Consum: React.FC = () => {
                 <Line data={dataMonat}/>
               </IonCardHeader>
               <IonCardContent>
-                <IonCardSubtitle>Stromverbrauch: {Math.round(vbr*4157)}kW</IonCardSubtitle>
-                <IonCardSubtitle>Stromproduktion: {Math.round(prd*120)}kW</IonCardSubtitle>
-                <IonCardSubtitle>Differenz: {Math.round(prd*120)-Math.round(vbr*4157)}kW</IonCardSubtitle>
+                <IonCardSubtitle>Stromverbrauch: {Math.round(vbr*4157)}kWh</IonCardSubtitle>
+                <IonCardSubtitle>Stromproduktion: {Math.round(prd*120)}kWh</IonCardSubtitle>
+                <IonCardSubtitle>Differenz: {Math.round(prd*120)-Math.round(vbr*4157)}kWh</IonCardSubtitle>
               </IonCardContent>
             </IonCard>
             <IonCard>
@@ -244,9 +244,9 @@ const Consum: React.FC = () => {
                 <Line data={dataJahr}/>
               </IonCardHeader>
               <IonCardContent>
-                <IonCardSubtitle>Stromverbrauch: {Math.round(vbr*50000)}kW</IonCardSubtitle>
-                <IonCardSubtitle>Stromproduktion: {Math.round(prd*1000)}kW</IonCardSubtitle>
-                <IonCardSubtitle>Differenz: {Math.round(prd*1000)-Math.round(vbr*50000)}kW</IonCardSubtitle>
+                <IonCardSubtitle>Stromverbrauch: {Math.round(vbr*50000)}kWh</IonCardSubtitle>
+                <IonCardSubtitle>Stromproduktion: {Math.round(prd*1000)}kWh</IonCardSubtitle>
+                <IonCardSubtitle>Differenz: {Math.round(prd*1000)-Math.round(vbr*50000)}kWh</IonCardSubtitle>
               </IonCardContent>
             </IonCard>
           </IonCol>
