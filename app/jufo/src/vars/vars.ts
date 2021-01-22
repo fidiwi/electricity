ebad26c179acc59db2b03202c763921548375ec6
fidiwi <friedrichleowinkler@gmx.de>
2021-01-22 19:36:26 +0100

    const haustyp werte

--- app/jufo/src/vars/vars.ts
+++ app/jufo/src/vars/vars.ts
@@ -8,6 +8,14 @@ export const getHaustyp = () => {
     return haustyp;
 };
 
+
 export const hausStrings = ["Apartmenthaus", "Reihenhaus", "Mehrfamilienhaus", "Einfamilienhaus"];
+
+export const hausVbr = [0.5, 0.064, 0.346, 0.09];
+
+export const prd = [12.5, 3.75, 8.75, 18.75];
+
+export const tagesVbr = [2.85, 0.37, 2, 0.51];
+
 
 export default null;
