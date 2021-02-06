f0d69e3fcb79f0dcfed988be46529c919d01b80a
KiKoLEL <kianmars177@gmail.com>
2021-02-06 16:23:51 +0100

    slides

--- app/jufo/src/pages/SlidesTest.tsx
+++ app/jufo/src/pages/SlidesTest.tsx
@@ -4,6 +4,8 @@ import './SlidesTest.css';
 import { arrowForward } from 'ionicons/icons';
 import logo from '../bilder/logo.svg'
 import modell from '../bilder/modell.jpg'
+import schieberegler from '../bilder/schieberegler.jpg'
+import dashboard from '../bilder/dashboard.jpg'
 
 // Optional parameters to pass to the swiper instance.
 // See http://idangero.us/swiper/api/ for valid options.
@@ -14,7 +16,7 @@ const slideOpts = {
 
 const SlidesTest: React.FC = () => (
   <IonContent fullscreen class="ion-padding">
-    <IonSlides pager={true} options={slideOpts}>
+    <IonSlides pager={false} options={slideOpts}>
       <IonSlide>
         <h1><IonText color="primary">  electri</IonText>
         <IonText color="dark">CITY</IonText></h1>
@@ -24,12 +26,19 @@ const SlidesTest: React.FC = () => (
       </IonSlide>
       <IonSlide>
         <img src={modell}/>
-        <h2>What is Ionic?</h2>
-        <p><b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.</p>
+        <h2>Die App</h2>
+        <p>Mit der <b>App</b> können sie mit dem <b>Modell</b> interagieren. Die Werte und Graphen zeigen die aktuellen Daten der Siedlung, da die App und die Siedlung mit einem <b>Server</b> in Verbindung stehen.</p>
       </IonSlide>
       <IonSlide>
-        <h2>Ready to Play?</h2>
+        <img src={schieberegler}/>
+        <h2>Interaktion mit der App</h2>
+        <p>Mit den <b>Schieberegler</b> können Sie die Modellwerte der App ändern. Dafür müssen Sie das von uns genannte <b>Passwort</b> eingeben. Das gleiche gilt für die <b>Einstellungen</b>, wenn Sie die Grundstücke ändern möchten.</p>
+      </IonSlide>
+      <IonSlide>
+        <img src={dashboard}/>
+        <h2>Viel Spaß beim Ausprobieren!</h2>
         <IonButton routerLink="/dashboard" fill="clear">Weiter zur Dashboard <IonIcon slot="end" icon={arrowForward}></IonIcon></IonButton>
+        <p><b>Info:</b> Es kann manchmal ein paar Sekunden dauern, bis Ihr Smartphone die Werte vom Server erhält. Wie bitten Sie um etwas Geduld.</p>
       </IonSlide>
     </IonSlides>
   </IonContent>
